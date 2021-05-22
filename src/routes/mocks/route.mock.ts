f799ff16bf995efd028a76ad2875e00254437195
greatSumini <greatsumini@gmail.com>
2021-05-23 03:33:19 +0900

    chore: :bento: update EDGE_DATA

--- src/routes/mocks/route.mock.ts
+++ src/routes/mocks/route.mock.ts
@@ -2,47 +2,31 @@ import { isObjectMatch } from '@src/common/helpers/is-object.match';
 import { RouteEdge } from '../entities/route-edge.entity';
 
 const EDGE_DATA = [
-  { lat: 37.5663, lng: 126.9926 },
-  {
-    lat: 37.56836300598753,
-    lng: 126.99238499785787,
-  },
-  {
-    lat: 37.56622522722469,
-    lng: 126.98973461590663,
-  },
-  {
-    lat: 37.568178079452956,
-    lng: 126.98949197530547,
-  },
+  { name: 'Cityhall', lat: 37.57015, lng: 126.98777 },
+  { name: 'Sinhan L Tower', lat: 37.57015, lng: 126.992213 },
+  { name: 'Eulji Starbucks', lat: 37.57049, lng: 126.99509 },
+  { name: 'Ducksu Elemental School', lat: 37.57015, lng: 126.997738 },
+  { name: 'Eulji-lo 3ga Station', lat: 37.567922, lng: 126.98777 },
+  { name: 'LG Twin Towers', lat: 37.567922, lng: 126.992213 },
+  { name: '63 Square', lat: 37.567922, lng: 126.995036 },
+  { name: 'Samsung Plaza', lat: 37.567922, lng: 126.997738 },
+  { name: "Summoner's Lift", lat: 37.566135, lng: 126.98777 },
+  { name: 'Sumin Home', lat: 37.566135, lng: 126.992213 },
+  { name: 'Kindergarten', lat: 37.566135, lng: 126.995036 },
+  { name: 'TeddyBear Museum', lat: 37.566135, lng: 126.997738 },
+  { name: 'Lotte world', lat: 37.564755, lng: 126.98777 },
+  { name: 'Timesqure', lat: 37.564755, lng: 126.992213 },
+  { name: 'JunctionX Hall', lat: 37.564755, lng: 126.995036 },
+  { name: 'Junsoo Home', lat: 37.564755, lng: 126.997738 },
+  { name: 'The Hyundai Seoul', lat: 37.562203, lng: 126.988812 },
+  { name: 'COEX', lat: 37.562402, lng: 126.992213 },
+  { name: '63 Building', lat: 37.562562, lng: 126.995656 },
+  { name: 'Whitehouse', lat: 37.562465, lng: 126.998449 },
+  { name: 'Asgard', lat: 37.561316, lng: 126.988932 },
+  { name: 'Coloseum', lat: 37.561219, lng: 126.992213 },
+  { name: 'Zootopia', lat: 37.561527, lng: 126.995938 },
+  { name: 'Yumin Home', lat: 37.562341, lng: 126.998462 },
 ];
-
-// [
-//   { name: 'Cityhall', lat: 37.57015, lng: 126.98777 },
-//   { name: 'Sinhan L Tower', lat: 37.57015, lng: 126.992213 },
-//   null,
-//   { name: 'Eulji Starbucks', lat: 37.57015, lng: 126.997738 },
-//   { name: 'Ducksu Elemental School', lat: 37.567922, lng: 126.98777 },
-//   { name: 'Eulji-lo 3ga Station', lat: 37.567922, lng: 126.992213 },
-//   { name: 'LG Twin Towers', lat: 37.567922, lng: 126.995036 },
-//   { name: '63 Square', lat: 37.567922, lng: 126.997738 },
-//   { name: 'Samsung Plaza', lat: 37.566135, lng: 126.98777 },
-//   { name: "Summoner's Lift", lat: 37.566135, lng: 126.992213 },
-//   { name: 'Sumin Home', lat: 37.566135, lng: 126.995036 },
-//   { name: 'Kindergarten', lat: 37.566135, lng: 126.997738 },
-//   { name: 'TeddyBear Museum', lat: 37.564755, lng: 126.98777 },
-//   { name: 'Lotte world', lat: 37.564755, lng: 126.992213 },
-//   { name: 'Timesqure', lat: 37.564755, lng: 126.995036 },
-//   { name: 'JunctionX Hall', lat: 37.564755, lng: 126.997738 },
-//   { name: 'Junsoo Home', lat: 37.562402, lng: 126.98777 },
-//   { name: 'The Hyundai Seoul', lat: 37.562402, lng: 126.992213 },
-//   null,
-//   null,
-//   { name: 'COEX', lat: 37.561219, lng: 126.98777 },
-//   { name: '63 Building', lat: 37.561219, lng: 126.992213 },
-//   null,
-//   null,
-// ];
 const WIDTH = Math.round(Math.sqrt(EDGE_DATA.length));
 
 const findDataIndex = (lat: number, lng: number): number =>
