71e812b3efd5c7b8dd290ff55b2cc6d67814ccc4
greatSumini <greatsumini@gmail.com>
2021-05-22 16:43:49 +0900

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
